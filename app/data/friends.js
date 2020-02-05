6a0671d51892bd5e944cfa9d0d9130a81ddf9f8f
scharman.hannah@gmail.com <scharman.hannah@gmail.com>
2020-02-05 15:34:58 -0700

    completed app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,89 @@
+//Here are all the potential friends the user can match with.
+//They are stored as an array. 
 
+var friends = [
+    {
+      name: "Katie",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/4/45/Katie_Melua-1050903.jpg",
+      scores: [
+        "5",
+        "2",
+        "3",
+        "3",
+        "5",
+        "1",
+        "2",
+        "4",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: "Natalie",
+      photo: "https://images.pexels.com/photos/1849002/pexels-photo-1849002.jpeg?cs=srgb&dl=close-up-photography-of-woman-wearing-black-framed-hippie-1849002.jpg&fm=jpg",
+      scores: [
+        "4",
+        "1",
+        "3",
+        "1",
+        "3",
+        "5",
+        "2",
+        "2",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: "Steven",
+      photo: "https://live.staticflickr.com/8295/7769753188_a8d03f5427_b.jpg",
+      scores: [
+        "4",
+        "1",
+        "2",
+        "3",
+        "1",
+        "4",
+        "5",
+        "2",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Jennifer",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Katie_Spotz.jpg",
+      scores: [
+        "1",
+        "3",
+        "1",
+        "4",
+        "5",
+        "2",
+        "3",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+      name: "Caden",
+      photo: "https://cdn.pixabay.com/photo/2019/05/21/19/43/squid-4219957_960_720.jpg",
+      scores: [
+        "3",
+        "3",
+        "2",
+        "5",
+        "5",
+        "3",
+        "2",
+        "4",
+        "1",
+        "3"
+      ]
+    }
+  ];
+  
+//Export friends array.
+  module.exports = friends;
+  
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+//Require express and path.
+var path = require('path');
+var express = require("express");
 
+//Set up variable to represent express.
+var app = express();
+
+//Set PORT.
+var PORT = process.env.PORT || 8080;
+
+//Use express.json and urlencoded.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//Require routes.
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//Start server listening at port. 
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
